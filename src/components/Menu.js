e8cafb99779223722805009d066d849a63796d74
nyx-6 <yin.bsun@outlook.com>
2021-01-12 19:48:29 -0600

    menu options updated

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,16 +10,17 @@ function Menu() {
 
         <nav className="menu">
             <ul className="menu__main_menu">
+            <li className="menu__option"><Link to={'/instructions'} className="menu__link">Instrucciones</Link></li>
+                <li className="menu__option"><Link to={'/exercises'} className="menu__link">Ejercicios</Link></li>
                 <li className="menu__option" onMouseEnter={() => { setSubMenuVisibility('visible') }}>
                     <a href="#" className="menu__link">Juegos</a>
                     <ul className="menu__submenu" style={{ visibility: subMenuVisibility }}
                         onMouseLeave={() => { setSubMenuVisibility('hidden') }}>
-                        <li><Link to={'/boomgame'} className="menu__link">Hacker Hero</Link></li>
                         <li><Link to={'/bubblesgame'} className="menu__link">Lluvia de burbujas</Link></li>
+                        <li><Link to={'/boomgame'} className="menu__link">Hacker Hero</Link></li>
                     </ul>
                 </li>
-                <li className="menu__option"><Link to={'/instructions'} className="menu__link">Instrucciones</Link></li>
-                <li className="menu__option"><Link to={'/exercises'} className="menu__link">Ejercicios</Link></li>
+               
             </ul>
         </nav>
     );
